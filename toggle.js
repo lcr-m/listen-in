bb132f3b60d0dd1a147062a4036a050fc1dbf5d5
marry <44299571+lcr-m@users.noreply.github.com>
2025-04-29 22:41:13 +0100

    Update toggle.js

--- toggle.js
+++ toggle.js
@@ -1,10 +1,6 @@
 document.querySelectorAll('.toggle-trigger').forEach(trigger => {
   trigger.addEventListener('click', () => {
-    const tracklist = trigger.nextElementSibling;
-    if (tracklist.style.display === "none" || tracklist.style.display === "") {
-      tracklist.style.display = "block";
-    } else {
-      tracklist.style.display = "none";
-    }
+    const tracklist = trigger.closest('.album-header').nextElementSibling;
+    tracklist.style.display = (tracklist.style.display === "none" || tracklist.style.display === "") ? "block" : "none";
   });
 });
