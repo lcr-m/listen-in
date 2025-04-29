c5bef6b1c9c9db3364e32983172793578cf845f7
marry <44299571+lcr-m@users.noreply.github.com>
2025-04-29 22:25:05 +0100

    Update toggle.js

--- toggle.js
+++ toggle.js
@@ -1,8 +1,10 @@
-<script>
-  document.querySelectorAll('.toggle-trigger').forEach(trigger => {
-    trigger.addEventListener('click', () => {
-      const details = trigger.closest('details');
-      details.open = !details.open;
-    });
+document.querySelectorAll('.toggle-trigger').forEach(trigger => {
+  trigger.addEventListener('click', () => {
+    const tracklist = trigger.nextElementSibling;
+    if (tracklist.style.display === "none" || tracklist.style.display === "") {
+      tracklist.style.display = "block";
+    } else {
+      tracklist.style.display = "none";
+    }
   });
-</script>
+});
